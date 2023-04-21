458c19ea5eed0c4d5b61be1c0b494721f747b535
Todd Butters <todd@butters.io>
2023-04-20 22:36:52 -0400

    fleek apply'd high bling

--- aliases.nix
+++ aliases.nix
@@ -1,5 +1,11 @@
 { pkgs, misc, ... }: {
    home.shellAliases = {
     fleeks = "cd /home/todd/.local/share/fleek";
+    
+    # bat --plain for unformatted cat
+    catp = "bat -P";
+    
+    # replace cat with bat
+    cat = "bat";
     };
 }
--- shell.nix
+++ shell.nix
@@ -1,4 +1,15 @@
-{ pkgs, misc, ... }: {# bash
+{ pkgs, misc, ... }: {
+    programs.exa.enableAliases = true;
+    
+    programs.exa.extraOptions = [
+   "--group-directories-first"
+   "--header"
+];
+    
+    programs.bat.config = {
+  theme = "TwoDark";
+};
+    # bash
   programs.bash.profileExtra = "[ -r ~/.nix-profile/etc/profile.d/nix.sh ] && source  ~/.nix-profile/etc/profile.d/nix.sh";
   programs.bash.initExtra = "source <(fleek completion bash)";
   programs.bash.enableCompletion = true;
